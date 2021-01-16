96420cd20c58311608e9a1e81cabba9d0dbbfef8
Promie Yutasane <promie.yutasane@gmail.com>
2021-01-16 18:18:09 +1100

    Implemented the telephoneValidator exercise and update test

--- src/telephoneValidator.ts
+++ src/telephoneValidator.ts
@@ -1,5 +1,7 @@
 const telephoneCheck = (str: string): boolean => {
-  return true
+  const re = /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/
+
+  return re.test(str)
 }
 
 export default { telephoneCheck }
